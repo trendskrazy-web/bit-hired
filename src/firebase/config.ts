b6bd9b5df34a590f68b7779cdd4c7fd482e259a5
KRAZY TRENDS <trendskrazy@gmail.com>
2025-11-13 14:26:20 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7245657825-227d1",
+  "appId": "1:264918620122:web:d4c36b52f90fb94c8ed2e3",
+  "apiKey": "AIzaSyBOpZZ60AxdGfrfhW-05patXw1RHP119Gk",
+  "authDomain": "studio-7245657825-227d1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "264918620122"
+};
