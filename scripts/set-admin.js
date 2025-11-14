a313d00fbe19ed03c84862f570148268b279301b
KRAZY TRENDS <trendskrazy@gmail.com>
2025-11-14 09:41:03 +0000

    i am unable to see the root directory

--- scripts/set-admin.js
+++ scripts/set-admin.js
@@ -1,3 +1,4 @@
+
 // This script is used to grant admin privileges to a user by setting a custom claim.
 // It uses the Firebase Admin SDK, which requires a service account key.
 
@@ -12,12 +13,23 @@ const path = require('path');
 const uid = process.argv[2]; 
 
 // Path to your service account key JSON file.
-const serviceAccountPath = path.resolve(__dirname, '../service-account.json');
-const serviceAccount = require(serviceAccountPath);
+// The script now looks for the key in the SAME directory as the script itself.
+const serviceAccountPath = path.resolve(__dirname, 'service-account.json');
+
+let serviceAccount;
+try {
+  serviceAccount = require(serviceAccountPath);
+} catch (error) {
+  console.error('\x1b[31m%s\x1b[0m', 'Error: Could not find or read service-account.json.');
+  console.error('Please make sure you have created a `service-account.json` file inside the `scripts` directory.');
+  console.error('You need to paste the content of the service account key you downloaded from Firebase into that file.');
+  process.exit(1);
+}
+
 
 // --- Main Logic ---
 if (!uid) {
-  console.error('Error: No user UID provided.');
+  console.error('\x1b[31m%s\x1b[0m', 'Error: No user UID provided.');
   console.log('Usage: node scripts/set-admin.js <user_uid>');
   process.exit(1);
 }
@@ -30,7 +42,7 @@ admin.initializeApp({
 // Set the custom claim { admin: true } on the specified user.
 admin.auth().setCustomUserClaims(uid, { admin: true })
   .then(() => {
-    console.log(`Successfully set admin claim for user: ${uid}`);
+    console.log('\x1b[32m%s\x1b[0m', `Successfully set admin claim for user: ${uid}`);
     console.log('They will have admin privileges on their next login.');
     process.exit(0);
   })
